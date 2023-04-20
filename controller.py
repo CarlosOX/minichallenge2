a42d1d73888a9fd0a1b5263cdb52bd0dfa093d97
CarlosOX <84168967+CarlosOX@users.noreply.github.com>
2023-04-20 16:35:00 -0600

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,151 +1,102 @@
-#!/user/bin/env python
-
-#!/usr/bin/env python  
-
-import rospy  
-
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist 
 
-from std_msgs.msg import Float32 
 
-import numpy as np 
+#Setup parameters, variables and callback functions here (if required)
+control_signal = 0.0
 
- 
+prev_error = 0.0
+integral_term = 0.0
+posicion_o = 0  #Posicion de salida de la planta
+reference = 0
 
-#This class will make the puzzlebot move following a square 
 
-class GoToGoal():  
+e=[0.0,0.0,0.0]
+#e[0] error actual    e[1] error anterior    e[2] error dos veces anterior
+u=[0.0,0.0]
+#u[0] salida actual    u[1] salida anterior
+Ts = 0.4
+#periodo de muestreo
 
-    def __init__(self):  
+#ganancias del modelo discreto
+kp = rospy.get_param("/proportional",0.0)
+ki = rospy.get_param("/integral",0.0)
+kd = rospy.get_param("/derivative",0.0)
 
-        rospy.on_shutdown(self.cleanup) 
+l = 1
 
-        ############ ROBOT CONSTANTS ################  
+K1=kp + Ts*ki + kd/Ts
+K2=-kp - 2.0*kd/Ts
+K3=kd/Ts
 
-        r=0.05 #wheel radius [m] 
+# definir la funcion de callback 
 
-        L=0.19 #wheel separation [m] 
 
-        theta = 0  #robot orientation in radians
+def callback_reference(msg):
+  global reference, reference_t
+  reference = msg.reference
+  reference_t = msg.time
 
-        x_k = 0  #robot position along the x-axis
+def callback_motor_output(msg):
+  global motor_o
+  motor_o = msg.data 
+  
+
+
+
+#Stop Condition
+def stop():
+ #Setup the stop message (can be the same as the control message)
+  print("Stopping")
+
+
+if __name__=='__main__':
+
+    #Initialise and Setup node
+    rospy.init_node("controller")
+    rate = rospy.Rate(150)
+    rospy.on_shutdown(stop)
+    rospy.Subscriber("/set_point", set_point, callback_reference)
+    rospy.Subscriber("/motor_output", Float32, callback_motor_output)
+    motor_input_pub = rospy.Publisher("/motor_input", Float32, queue_size=10)
+
+    #motor_o = motor_o / 13.08996939
+    #reference = reference / 13.08996939
+    
+    #Definir el error inicial
+    prev_error = 0
+    integral_term = 0
+
+
+
+    print("The Controller is Running")
+    #Run the node
+    while not rospy.is_shutdown():
+        #Write your code here
         
-        y_k = 0 # robot position along the y-axis
-
-        v = 0.0 # robot linear speed (m/s)
-
-        w = 0.0 #robot angular speed (rad/s)
-
-        self.wl = 0
-        self.wr = 0
-
-
-
-        while rospy.get_time() == 0:
-            print("no simulated time has been received yet")
-        print("Got time")
-
-
-        previous_time = rospy.get_time()
-
-        ###########  INIT PUBLISHERS ################ 
-
-        self.pub_cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)  
-
-        ############## SUBSCRIBERS ##################  
-
-        rospy.Subscriber("wl", Float32, self.wl_cb)  
-
-        rospy.Subscriber("wr", Float32, self.wr_cb)  
-
-        x_G = 1.0
-        y_G=  -1.0
-
-        v_msg=Twist() 
-
-        rate = rospy.Rate(20) #20Hz  
-
+        e[0]=reference - motor_o
+        u[0]=K1*e[0]+K2*e[1]+K3*e[2]+u[1]
         
 
-        print("Node initialized") 
+        if(u[0]>l):
+          u[0]=l
+        if(u[0]<-l):
+          u[0]=-l
 
-        while not rospy.is_shutdown():  
+        e[2]=e[1]
+        e[1]=e[0]
+        u[1]=u[0]
 
-            delta_t = rospy.get_time() - previous_time
-
-            previous_time = rospy.get_time()
-
-            v = r * (self.wr + self.wl) / 2.0
-
-            w = r * (self.wr - self.wl) / L
-
-            theta = theta + w * delta_t
-            
-            theta = np.arctan2(np.sin(theta),np.cos(theta))
-            
-
-            x_k = x_k + v * np.cos(theta) * delta_t
-
-            y_k = y_k + v *np.sin(theta) * delta_t
-
-            #distance to goal
-
-            d = np.sqrt((x_G-x_k)**2 + (y_G-y_k)**2)
-
-            #ANgle to the goal
-            theta_g = np.arctan2(y_G - y_k , x_G - x_k)
-
-            etheta = theta_g - theta
+        
+        #rospy.loginfo(control_signal)
+        
+        
+        motor_i = u[0]
+        #motor_i.time = rospy.get_time()
+        motor_input_pub.publish(motor_i)
+       
 
 
-             
-            print("x_k : " + str(x_k))
-            print("y_k : " + str(x_k))
-            print("theta : " + str(theta))
-
-            print("e_theta"+ str(etheta))
-            print("d: "+ str(d))
-
-
-            #pub_cmd_vel.publish(v_msg) #publish the robot's speed  
-
-            rate.sleep()  
-
-           
-    def wl_cb(self, wl):  
-
-        ## This function receives the left wheel speed from the encoders  
-
-        self.wl = wl.data 
-
-         
-
-    def wr_cb(self, wr):  
-
-        ## This function receives the right wheel speed from the encoders 
-
-        self.wr = wr.data  
-
-         
-
-    def cleanup(self):  
-
-        #This function is called just before finishing the node  
-
-        # Y ou can use it to clean things up before leaving  
-
-        # Example: stop the robot before finishing a node.    
-
-        vel_msg = Twist() 
-
-        self.pub_cmd_vel.publish(vel_msg) 
-
- 
-
-############################### MAIN PROGRAM ####################################  
-
-if __name__ == "__main__":  
-
-    rospy.init_node("go_to_goal", anonymous=True)  
-
-    GoToGoal() 
